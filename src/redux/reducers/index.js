f29cd7a765b75a7f156b624a9d5257c3c6c0b06d
Juan <juanpa_solano@hotmail.com>
2016-09-02 15:32:46 -0500

    bug fix

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,17 +1,27 @@
 
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
+import _ from 'lodash';
 
 const lists = (state = [], action) => {
   switch (action.type) {
+
     case 'CONNECTING_TO_LISTS':
       return state;
+
     case 'SET_LISTS':
       return action.payload;
+
     case 'CHANGEFEED_LIST':{
-      let newList = action.payload.new_val;
-      let newState = state.map(item => (item.id === newList.id) ? newList : item)
-      return newState
+      let {new_val, old_val} = action.payload;
+      if(new_val === null){ //list was removed
+        return state.filter(item => item.id !== old_val.id);
+      } else if(old_val === null) { //list was added
+        return [...state, new_val];
+      }else { //List was modified
+        return state.map(item => (item.id === new_val.id) ? new_val : item)
+      }
     }
+
     default:
       return state;
   }
